eb78aa68b029e26f34ca393349093ae9d4e672af
sonup309 <sonupatil1922@gmail.com>
2023-03-07 13:15:12 +0530

    first commit

---
+++ som.js
@@ -0,0 +1,8 @@
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[2].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+for(var i=0;i<li.length;i++){
+    ii[i].style.backgroundColor='#ccc';
+}
