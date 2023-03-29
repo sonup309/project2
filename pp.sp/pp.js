c11c6dbdab9a829bd77d63f14439aa5b694527db
sonup309 <sonupatil1922@gmail.com>
2023-03-29 09:49:27 +0530

    newp

---
+++ pp.sp/pp.js
@@ -0,0 +1,7 @@
+var items=document.getElementsByClassName('list-group-item')
+//console.log(items);
+//console.log(items[1]);
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
